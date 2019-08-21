8c651ad48f8064da59f601403d43c01e0387dea3
Quentin Ng <qng1997@gmail.com>
2019-08-21 10:40:58 -0700

    validation msgs

--- Models/Wedding.cs
+++ Models/Wedding.cs
@@ -8,15 +8,15 @@ namespace WeddingPlanner.Models
     {
         [Key]
         public int wedding_id {get;set;}
-        [Required]
+        [Required(ErrorMessage="Please enter the Bride's name.")]
         public string bride_name{get;set;}
-        [Required]
+        [Required(ErrorMessage="Please enter the Groom's name.")]
         public string groom_name{get;set;}
 
-        [Required]
+        [Required(ErrorMessage="Please select the wedding date.")]
         [DateCheck]
         public DateTime wedding_date{get;set;}
-        [Required]
+        [Required(ErrorMessage="Please enter the wedding venue")]
         public string wedding_address{get;set;}
         
         //Navigation properties
